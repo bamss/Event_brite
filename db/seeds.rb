1a66f3a93bcf08167ca14fa116f41f089d306e23
bamss <amadougolfa98@gmail.com>
2019-02-11 15:53:33 +0100

    ajout du mailer

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,5 @@ Event.destroy_all
 5.times do 
   @user = User.create(first_name: Faker::FunnyName.name, last_name: Faker::FunnyName.two_word_name, email: "#{Faker::Nation.nationality}@yoplait.com", description: "bammss t bo")
   @event = Event.create(start_date: Faker::Date.forward(23), duration: 45, title:  Faker::DragonBall.character, description: Faker::BojackHorseman.tongue_twister, price: 100, location: Faker::Address.state, admin: User.all.sample)
-  @Attendance = Attendance.create(stripe_customer_id: rand(100..200),event:Event.all.sample, attendant:User.all.sample   )
+  @Attendance = Attendance.create(stripe_customer_id: rand(100..200), event: Event.all.sample, attendant: User.all.sample   )
 end
