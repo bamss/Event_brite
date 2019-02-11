c4761214f916eb395dac5036e11d07b7f1612f64
bamss <amadougolfa98@gmail.com>
2019-02-11 14:58:04 +0100

    modification venant de Bamss le BG

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,10 @@
 require 'faker'
+require 'as-duration'
 User.destroy_all
 Attendance.destroy_all
 Event.destroy_all
 
 5.times do 
   @user = User.create(first_name: Faker::FunnyName.name, last_name: Faker::FunnyName.two_word_name, email: "#{Faker::Nation.nationality}@yoplait.com", description: "bammss t bo")
+  @event = Event.create(start_date: Faker::Date.forward(23), duration: 45, title:  Faker::DragonBall.character, description: Faker::BojackHorseman.tongue_twister, price: 100, location: Faker::Address.state, admin: User.all.sample)
 end
